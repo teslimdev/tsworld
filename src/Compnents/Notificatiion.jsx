959a17845404d0624b9c6dbdabb407a4b4a1786b
lord-space <teslimabdullah2@gmail.com>
2024-06-15 10:13:18 +0100

    overall update

--- src/Compnents/Notificatiion.jsx
+++ src/Compnents/Notificatiion.jsx
@@ -19,19 +19,19 @@ const Notification = () => {
           <div className='max-w-[400px] md:max-w-[700px] lg:max-w-[900px] m-auto fixed top-16 lg:top-36 px-4'>
             <div className='h-[35rem] rounded-lg lg:h-[30rem] shadow-2xl overflow-y-scroll' style={{ scrollbarWidth: 'none' }}>
               <div className='bg-gray-200 rounded-lg m-auto max-w-[400px] md:max-w-[700px] max-h-full h-full overflow-auto lg:max-w-[900px] sl:mr-[-17px]'>
-                <div className='bg-gray-400 py-2 rounded-tr-lg rounded-tl-lg text-center text-white sticky top-0 w-full max-w-[400px] md:max-w-[700px] lg:max-w-[900px]'>
-                  <h2>Welcome To TS-WORLD</h2>
+                <div className='bg-gray-900 py-2 rounded-tr-lg rounded-tl-lg text-center text-gray-200 sticky top-0 w-full max-w-[400px] md:max-w-[700px] lg:max-w-[900px]'>
+                  <h2 className=' font-bold '>Welcome To TS-WORLD</h2>
                 </div>
-                <div className='px-4'>
-                  <h3 className='text-center'>Announcement!!!</h3>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
+                <div className='px-4 pb-4'>
+                  <h3 className='text-center py-3 text-xl'>Announcement!!!</h3>
+                  <p className=' py-2 text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
+                  <p className=' py-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
+                  <p className=' py-2 text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
+                  <p className=' py-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
+                  <p className=' py-2 text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
+                  <p className=' py-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
+                  <p className=' py-2 text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam quo? Sapiente ducimus impedit maxime itaque error nobis facere, quos repellat laboriosam reprehenderit possimus natus magnam. Nostrum eaque debitis accusantium?</p>
+                  <p className=' py-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo labore, maiores praesentium dolorum nostrum voluptatum assumenda accusamus laboriosam nesciunt.</p>
                 </div>
                 <div className='sticky bottom-0'>
                   <button onClick={handleCloseClick} className='bg-gray-800 w-full uppercase py-2 text-white rounded-br-lg rounded-bl-lg'>close</button>
